d2a50fd97c751366654a1706e4cfd9a9af52a08f
AlexanderJChoi <alexanderajchoi@gmail.com>
2021-05-10 21:51:00 -0700

    Add custom puzzle, duplicate state catching, looping puzzle solving, and fixed heuristics.

--- solve.py
+++ solve.py
@@ -38,6 +38,9 @@ depth_24_index = (0,0)
 # this is the min priority queue data structure
 min_heap = []
 
+# We use a dictionary to determine repeated states
+dictionary = {}
+
 # Prints a matrix in the square puzzle format
 def print_matrix(matrix):
     print("")
@@ -53,10 +56,35 @@ def copy_matrix(m1, m2):
     for i in range(len(m1)):
         m2.append(m1[i].copy())
 
+# Create a unique hash value for a matrix
+def hash_matrix(matrix):
+    string = ""
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            string+=(str(matrix[i][j]))
+    return string
+
 # Swaps matrix entries at the two indices
 # Each index should be a pair, to properly index
 def swap_entries(matrix, index1, index2):
     matrix[index1[0]][index1[1]],matrix[index2[0]][index2[1]] = matrix[index2[0]][index2[1]],matrix[index1[0]][index1[1]] 
+
+# Interface to create a custom puzzle
+def custom_puzzle():
+    print("Enter your puzzle, using 0 to represent the blank space. Enter only a single element at a time, pressing <ENTER> after each element.")
+    puzzle = []
+    index = (-1, -1)
+    for i in range(puzzle_size):
+        print("Enter row " + str(i) + ":")
+        next_line = []
+        for j in range (puzzle_size):
+            num = int(input())
+            next_line.append(num)
+            if num == 0:
+                index = (i, j)
+        puzzle.append(next_line)
+
+    return puzzle, index
 
 # We describe the operators in terms of moving the blank space
 # state is a matrix describing a state of the game
@@ -149,7 +177,16 @@ def initialize_start_node(state, index, heuristic_func ):
     new_node = (priority, depth, state, index)
     heapq.heappush(min_heap, new_node)
 
+    hash_val = hash_matrix(state)
+    dictionary[hash_val] = 1
 
+# Empties heap to make ready for new solve
+def reset_solution():
+    while len(min_heap) != 0:
+        heapq.heappop(min_heap)
+    dictionary.clear()
+
+# Here, we solve a puzzle, using a given heuristic function
 def solve_puzzle(state, index, heuristic_func ):
     initialize_start_node(state, index, heuristic_func)
     nodes_expanded = 0
@@ -178,15 +215,20 @@ def solve_puzzle(state, index, heuristic_func ):
         nn_depth = cn_depth + 1
         new_states = get_adjacent_states(cn_state, cn_index)
         for new_state, new_index in new_states:
-            nn_priority = heuristic_func(new_state) + nn_depth
-            new_node = (nn_priority, nn_depth, new_state, new_index)
-            heapq.heappush(min_heap, new_node)
+            hash_val = hash_matrix(new_state)
+
+            if hash_val not in dictionary:
+                nn_priority = heuristic_func(new_state) + nn_depth
+                new_node = (nn_priority, nn_depth, new_state, new_index)
+                heapq.heappush(min_heap, new_node)
+                
+                dictionary[hash_val] = 1
 
         nodes_expanded+=1
         if len(min_heap) > max_nodes:
             max_nodes = len(min_heap)
 
-
+# Here we define each heuristic
 def uniform_cost_heuristic(state):
     return 0
 
@@ -194,7 +236,7 @@ def misplaced_tile_heuristic(state):
     count = 0
     for i in range(len(state)):
         for j in range(len(state)):
-            if state[i][j] != solution_state[i][j]:
+            if state[i][j] != solution_state[i][j] and state[i][j] != 0:
                 count+=1
 
     return count
@@ -205,11 +247,76 @@ def manhattan_distance_heuristic(state):
         for j in range(len(state)):
             for k in range(len(solution_state)):
                 for l in range(len(solution_state)):
-                    if state[i][j] == solution_state[k][l]:
+                    if state[i][j] == solution_state[k][l] and state[i][j] != 0:
                         distance = abs(i-k) + abs(j-l)
                         count += distance
 
     return count
 
-solve_puzzle(test_puzzle, index_blank, uniform_cost_heuristic)
+# Below, is the interface to initialize the puzzle to use, and decide which heuristic to use
+while(1):
+    chosen_puzzle = []
+    chosen_heuristic = uniform_cost_heuristic
+    print("Let's Solve an 8 Puzzle!!")
+    a = """Which puzzle would you like to use?
+            (0) Use a custom puzzle
+            (1) Use a 0-depth puzzle
+            (2) Use a 2-depth puzzle
+            (3) Use a 4-depth puzzle
+            (4) Use a 8-depth puzzle
+            (5) Use a 12-depth puzzle
+            (6) Use a 16-depth puzzle
+            (7) Use a 20-depth puzzle
+            (8) Use a 24 depth puzzle
+            """
+    puzzle_option = int(input(a))
 
+    if puzzle_option == 0:
+        chosen_puzzle, chosen_index = custom_puzzle()
+    elif puzzle_option == 1:
+        copy_matrix(depth_0_puzzle, chosen_puzzle)
+        chosen_index = depth_0_index
+    elif puzzle_option == 2:
+        copy_matrix(depth_2_puzzle, chosen_puzzle)
+        chosen_index = depth_2_index
+    elif puzzle_option == 3:
+        copy_matrix(depth_4_puzzle, chosen_puzzle)
+        chosen_index = depth_4_index
+    elif puzzle_option == 4:
+        copy_matrix(depth_8_puzzle, chosen_puzzle)
+        chosen_index = depth_8_index
+    elif puzzle_option == 5:
+        copy_matrix(depth_12_puzzle, chosen_puzzle)
+        chosen_index = depth_12_index
+    elif puzzle_option == 6:
+        copy_matrix(depth_16_puzzle, chosen_puzzle)
+        chosen_index = depth_16_index
+    elif puzzle_option == 7:
+        copy_matrix(depth_20_puzzle, chosen_puzzle)
+        chosen_index = depth_20_index
+    elif puzzle_option == 8:
+        copy_matrix(depth_24_puzzle, chosen_puzzle)
+        chosen_index = depth_24_index
+    else:
+        print("OPTION NOT RECOGNIZED. RESTARTING")
+        continue
+
+
+    a = """Which heuristic would you like to use?\n
+            (1) No Heuristic (Uniform Cost Search)
+            (2) Misplaced Tile Heuristic
+            (3) Manhattan Distance Heuristic
+            """
+    heuristic_option = int(input(a))
+    if heuristic_option == 1:
+        chosen_heuristic = uniform_cost_heuristic
+    elif heuristic_option == 2:
+        chosen_heuristic = misplaced_tile_heuristic
+    elif heuristic_option == 3:
+        chosen_heuristic = manhattan_distance_heuristic
+    else:
+        print("OPTION NOT RECOGNIZED. RESTARTING")
+
+
+    solve_puzzle(chosen_puzzle, chosen_index, chosen_heuristic)
+    reset_solution()
